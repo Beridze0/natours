d9ef40aa217347f5bdd68e9a489fcadacdf9fe1c
Beridze0 <beridzedavit904@gmail.com>
2025-09-30 20:36:29 +0400

    popst req

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-const express = require('express');
-const fs = require('fs');
-
-const app = express();
-
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -12,6 +7,12 @@ const app = express();
 // app.post('/', (req, res) => {
 //   res.send('You can post to this endpoint...');
 // });
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -25,6 +26,27 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
