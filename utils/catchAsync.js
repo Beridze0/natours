a65df308114954a2e55a096abc7dc8c83629cfc0
Beridze0 <beridzedavit904@gmail.com>
2025-10-10 20:39:51 +0400

    Adding 404 Not Found Errors

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,6 +1,1 @@
-//eslint-disable-next-line
-module.exports = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next);
-  };
-};
+module.exports = (fn) => (req, res, next) => fn(req, res, next).catch(next);
