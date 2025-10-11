b4f734af0618a9229dc93f5e220279ca9afcd258
Beridze0 <beridzedavit904@gmail.com>
2025-10-11 18:02:33 +0400

    Catching Uncaught Exceptions

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -54,6 +54,7 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    //eslint-disable-next-line
     let error = { ...err };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
